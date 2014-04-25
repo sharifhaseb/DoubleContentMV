1fd0d1bba2ee55bbba223bb42635ed100ef5e25b
root <root@PI3B01.(none)>
2014-04-25 10:26:52 +0000

    Allows frame dropping.

--- VideoShow.sh
+++ VideoShow.sh
@@ -3,10 +3,10 @@ sudo pkill fbi
 sudo pkill mplayer
 sudo rm /home/pi/log*
 sudo python StartNow.py
-sudo mplayer -vo fbdev:/dev/fb1 -fs -nosound /home/pi/pi3b/Content/tty1.avi >> /home/pi/log1.txt &
-sudo mplayer -vo fbdev:/dev/fb2 -fs -nosound /home/pi/pi3b/Content/tty2.avi >> /home/pi/log2.txt &
-sudo mplayer -vo fbdev:/dev/fb3 -fs -nosound /home/pi/pi3b/Content/tty3.avi >> /home/pi/log3.txt &
-sudo mplayer -vo fbdev:/dev/fb4 -fs -nosound /home/pi/pi3b/Content/tty4.avi >> /home/pi/log4.txt &
-sudo mplayer -vo fbdev:/dev/fb5 -fs -nosound /home/pi/pi3b/Content/tty5.avi >> /home/pi/log5.txt &
-sudo mplayer -vo fbdev:/dev/fb0 -fs -nosound /home/pi/pi3b/Content/tty0.avi >> /home/pi/log0.txt &
+sudo mplayer -framedrop -vo fbdev:/dev/fb1 -nosound /home/pi/pi3b/Content/tty1.avi >> /home/pi/log1.txt &
+sudo mplayer -framedrop -vo fbdev:/dev/fb2 -nosound /home/pi/pi3b/Content/tty2.avi >> /home/pi/log2.txt &
+sudo mplayer -framedrop -vo fbdev:/dev/fb3 -nosound /home/pi/pi3b/Content/tty3.avi >> /home/pi/log3.txt &
+sudo mplayer -framedrop -vo fbdev:/dev/fb4 -nosound /home/pi/pi3b/Content/tty4.avi >> /home/pi/log4.txt &
+sudo mplayer -framedrop -vo fbdev:/dev/fb5 -nosound /home/pi/pi3b/Content/tty5.avi >> /home/pi/log5.txt &
+sudo mplayer -framedrop -vo fbdev:/dev/fb0 -nosound /home/pi/pi3b/Content/tty0.avi >> /home/pi/log0.txt &
 
