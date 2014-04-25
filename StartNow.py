1ac333ee670d75326e61430111d303e2ae21be11
root <root@PI3B02.(none)>
2014-04-25 08:32:46 +0000

    A fix to video timer, it waits longer if the next minute is too close.

--- StartNow.py
+++ StartNow.py
@@ -12,8 +12,13 @@ def main():
     now = datetime.datetime.now()
     # Print the current time.
     print 'Current time is ', now.strftime("%Y-%m-%d %H:%M")
-    # Desired minute.
-    td  = (now.minute + 1 ) % 60
+    # If the next minute is too close than jump to the other one.
+    if 60 -  now.second < 10:
+       a = 2
+    else:
+       a = 1
+    # Desired minute.    
+    td  = (now.minute + a ) % 60
     # Loop to check if desired time is reached.
     while td != now.minute :
         # Get the current time to check in the cycle
